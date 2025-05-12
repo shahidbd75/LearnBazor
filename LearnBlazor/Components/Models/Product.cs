2f91a8ebec720e5dada27f29f8c800c3f83426ac
Md. Abdul Baset Shahid <abdul.baset@niftycoders.com>
2025-05-12 16:23:04 +0600

    Add project files.

---
+++ LearnBlazor/Components/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace LearnBlazor.Components.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Boolean IsAvailable { get; set; }
+    }
+}
