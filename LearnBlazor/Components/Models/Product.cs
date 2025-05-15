4e1a62ed7f893a5d3fde3b60beda220e3c2c2c0a
Md. Abdul Baset Shahid <abdul.baset@niftycoders.com>
2025-05-15 16:18:30 +0600

    Add validation summery and individual message

--- LearnBlazor/Components/Models/Product.cs
+++ LearnBlazor/Components/Models/Product.cs
@@ -1,16 +1,34 @@
-﻿namespace LearnBlazor.Components.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnBlazor.Components.Models
 {
     public class Product
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
 
+        [Range(1,1000)]
         public decimal Price { get; set; }
 
         public Boolean IsAvailable { get; set; }
 
         public IEnumerable<ProductVarient> ProductVarients { get; set; }
+
+        public Category Catergory { get; set; }
+
+        public DateOnly OfferEnd { get; set; }
+    }
+
+    public enum Category
+    {
+        Electronics,
+        Clothing,
+        Food,
+        Toys,
+        Books
     }
 }
