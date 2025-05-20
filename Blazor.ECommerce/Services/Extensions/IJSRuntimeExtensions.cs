4ec5ed9fe807e32605f5ee089eec2088f8a07c76
Md. Abdul Baset Shahid <abdul.baset@niftycoders.com>
2025-05-20 16:21:33 +0600

    Toastr Added

---
+++ Blazor.ECommerce/Services/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.JSInterop;
+
+namespace Blazor.ECommerce.Services.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+        public static async Task ToastrInfo(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "info", message);
+        }
+
+        public static async Task ToastrWarning(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToastr", "warning", message);
+        }
+    }
+}
---
+++ Blazor.ECommerce/wwwroot/js/notification.js
@@ -0,0 +1,33 @@
+﻿window.ShowToastr = (type, message) => {
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "500",
+        "timeOut": "2000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    switch (type) {
+        case 'success':
+            toastr.success(message);
+            break;
+        case 'info':
+            toastr.info(message);
+            break;
+        case 'warning':
+            toastr.warning(message);
+            break;
+        case 'error':
+            toastr.error(message);
+            break;
+    }
+}
