cd1d3db9bea9bc67975620923580c1d37eb3da1d
Md. Abdul Baset Shahid <abdul.baset@niftycoders.com>
2025-06-11 14:59:23 +0600

    Authorize navbar

---
+++ Blazor.ECommerce/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,37 @@
+﻿using Blazor.ECommerce.Constants;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Blazor.ECommerce.Components.Layout
+{
+    public partial class MainLayout(AuthenticationStateProvider authenticationStateProvider)
+    {
+        private bool _IsNavbarVisible;
+        protected override async Task OnInitializedAsync()
+        {
+            _IsNavbarVisible = await ShowNavbarAsync();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            _IsNavbarVisible = await ShowNavbarAsync();
+        }
+
+        public async Task<bool> ShowNavbarAsync()
+        {
+            var userState = await authenticationStateProvider.GetAuthenticationStateAsync();
+            var user = userState.User;
+
+            if (user.Identity?.IsAuthenticated ==false)
+            {
+                return true;
+            }
+
+            if (!user.IsInRole(RoleData.Admin))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
