df24a92a3b5942eb4123cae89088b6c64cf883ea
Md. Abdul Baset Shahid <abdul.baset@niftycoders.com>
2025-06-11 12:41:17 +0600

    Add Role and Redesign Register

---
+++ Blazor.ECommerce/Constants/RoleData.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor.ECommerce.Constants
+{
+    public class RoleData
+    {
+        public static string Admin = "Admin";
+        public static string Customer = "Customer";
+    }
+}
